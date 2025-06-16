b18547362a08551cdfeada0f1893f9684f0fd044
Tuan Le <leanhtuan030523@gmail.com>
2025-06-16 13:51:23 +0700

    feat: initialize FastAPI app with auth and user routers, and create database tables

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.routers.auth import router as auth_router
+from app.routers.user import router as user_router
+
+from app.db.database import Base, engine
+from app.db.models import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(user_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
