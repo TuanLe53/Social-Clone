75d2b5c48a7229aeca876dfa2a4f9ac7d9356200
Tuan Le <leanhtuan030523@gmail.com>
2025-06-16 13:48:50 +0700

    feat(user): implement follow and unfollow endpoints with follower/following retrieval

---
+++ app/routers/user.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import Annotated
+from uuid import UUID
+
+from app.db.database import get_db
+from app.db.models import User
+from app.schemas.user import UserProfile
+from app.dependencies import get_current_user
+from app.services.user import follow_user, unfollow_user, get_followers_by_user_id, get_following_by_user_id
+
+router = APIRouter(
+    prefix="/user",
+)
+
+@router.post("/follow")
+async def following_user(user: Annotated[User, Depends(get_current_user)], following_id: UUID, db: Session = Depends(get_db)):
+    if user.id == following_id:
+        raise HTTPException(status_code=400, detail="You cannot follow yourself.")
+    
+    following_user = follow_user(db, user.id, following_id)
+    
+    return {"message": f"User {user.username} followed user with username: {following_user.username}"}
+
+@router.delete("/follow")
+async def unfollowing_user(user: Annotated[User, Depends(get_current_user)], following_id: UUID, db: Session = Depends(get_db)):
+    if user.id == following_id:
+        raise HTTPException(status_code=400, detail="You cannot unfollow yourself.")
+    
+    unfollowed_user = unfollow_user(db, user.id, following_id)
+    
+    return {"message": f"User {user.username} unfollowed user with username: {unfollowed_user.username}"}
+
+@router.get("/followers", response_model=list[UserProfile])
+async def get_followers(user_id: UUID, current_user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+    followers = get_followers_by_user_id(db, user_id)
+    return followers
+
+@router.get("/followings", response_model=list[UserProfile])
+async def get_followers(user_id: UUID, current_user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+    followings = get_following_by_user_id(db, user_id)
+    return followings
