603e2eb5a22c496e481a85c96162566702a92f8c
Tuan Le <leanhtuan030523@gmail.com>
2025-06-25 17:37:38 +0700

    feat(auth): create and store refresh token in DB with expiration metadata

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,12 @@
 import os
 import jwt
+import uuid
 from dotenv import load_dotenv
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
+from sqlalchemy.orm import Session
+
+from app.db.models import RefreshToken
 
 load_dotenv()
 
@@ -30,3 +34,25 @@ def create_access_token(data: dict, expired_delta: timedelta | None = None) -> s
     
     return encoded_jwt
     
+def create_refresh_token(db_session: Session, data: dict, expired_delta: timedelta | None = None) -> str:
+    to_encode = {"refresh_token_id": str(uuid.uuid4())}
+
+    if expired_delta is not None:
+        expire = datetime.now(timezone.utc) + expired_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(days=7)
+        
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    db_token = RefreshToken(
+        id= to_encode["refresh_token_id"],
+        user_id=data.get("sub"),
+        expires = expire
+    )
+    
+    db_session.add(db_token)
+    db_session.commit()
+    db_session.refresh(db_token)
+    
+    return encoded_jwt
