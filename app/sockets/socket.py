5a015af7fade202489a4776238dfe0219df00757
Tuan Le <leanhtuan030523@gmail.com>
2025-07-01 19:12:44 +0700

    Create a simple socket

---
+++ app/sockets/socket.py
@@ -0,0 +1,12 @@
+import socketio
+
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=[])
+sio_app = socketio.ASGIApp(sio, socketio_path='/ws/socket.io')
+
+@sio.event
+async def connect(sid, environ):
+    print(f"Client connected: {sid}")
+    
+@sio.event
+async def disconnect(sid):
+    print(f"Client disconnected from default namespace: {sid}")
