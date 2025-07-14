edd7e6683021145a91420841ee69513f9b78d3b7
Tuan Le <leanhtuan030523@gmail.com>
2025-07-14 19:02:15 +0700

    - Introduced Dialog UI with tab buttons for followers and followings - Added local state to manage selected tab - Integrated FollowList component to display user relationships based on selection - Enhanced profile interactivity for better user experience

--- frontend/src/routes/profile.$username.tsx
+++ frontend/src/routes/profile.$username.tsx
@@ -1,7 +1,11 @@
 import { getProfile } from '@/api/user';
 import FollowButton from '@/components/FollowButton';
+import FollowList from '@/components/FollowList';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { useAuth } from '@/contexts/auth';
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react';
 
 export const Route = createFileRoute('/profile/$username')({
     component: RouteComponent,
@@ -26,6 +30,11 @@ export const Route = createFileRoute('/profile/$username')({
 function RouteComponent() {
     const { isAuthenticated } = useAuth();
     const { user, error } = Route.useLoaderData();
+    console.log(user);
+    const [selectedTab, setSelectedTab] = useState<'followers' | 'followings'>('followers');
+    const handleTabChange = (tab: 'followers' | 'followings') => {
+        setSelectedTab(tab);
+    }
 
     if (error) {
         return (
@@ -38,6 +47,17 @@ function RouteComponent() {
     return (
         <div>
             <h1>{user.name || user.username}'s Profile</h1>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button type='button' onClick={() => handleTabChange('followers')}>128 followers</Button>
+                </DialogTrigger>
+
+                <DialogTrigger asChild>
+                    <Button type='button' onClick={() => handleTabChange('followings')}>125 followings</Button>
+                </DialogTrigger>
+
+                <FollowList selectedTab={selectedTab} user_id={user.id} />
+            </Dialog>
             {isAuthenticated && <FollowButton user={user}/>}
             
         </div>
