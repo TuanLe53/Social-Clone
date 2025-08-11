84442f5fa33c0fa7aedfc895c6d98b1e2a420f8a
Tuan Le <leanhtuan030523@gmail.com>
2025-08-11 22:43:24 +0700

    ui(user-card): add avatar with fallback icon and hover styling

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,5 +1,7 @@
 import type { UserProfile } from "@/types/user";
 import { Link } from "@tanstack/react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { CircleUserRound } from "lucide-react";
 
 interface UserCardProps{
     user: UserProfile
@@ -8,7 +10,13 @@ interface UserCardProps{
 export default function UserCard({user}: UserCardProps) {
     
     return (
-        <div>
+        <div className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors">
+            <Avatar>
+                <AvatarImage src={user.avatar_url} />
+                <AvatarFallback>
+                    <CircleUserRound />
+                </AvatarFallback>
+            </Avatar>
             <Link to="/profile/$username" params={{username: user.username}}>
                 <p>{user.username}</p>
             </Link>
