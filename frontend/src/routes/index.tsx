ec7bdc3a6976487f8bfb7fdfea90806ab2977a59
Tuan Le <leanhtuan030523@gmail.com>
2025-08-11 21:49:32 +0700

    Adds a "Social" heading with updated styling.
    Introduces an "Or continue with" separator line for a cleaner layout.
    
    Adds a "Login with GitHub" button with a GitHub SVG icon.
    
    Updates form fields with improved spacing and removes redundant FormDescription components.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,5 @@
+import LoginForm from '@/components/LoginForm';
+import { useAuth } from '@/contexts/auth'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -5,10 +7,23 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
+  const { isAuthenticated } = useAuth();
 
   return (
-    <div className="text-center">
-      <h1>Placeholder</h1>
+    <div className='flex flex-row items-center justify-center'>
+      {isAuthenticated
+        ? 
+        <h1>Placeholder</h1>      
+        :
+        <div className="flex gap-4 lg:min-h-svh lg:max-w-1/2 flex-row lg:items-center justify-center p-6 md:p-10">
+          <div className="w-full max-w-sm md:max-w-3xl">
+            <LoginForm />
+          </div>
+          <div className="block hidden lg:block bg-blue-300 w-full max-w-sm md:max-w-3xl">
+            <img src="https://placehold.co/600x400" alt="Placeholder image" />
+          </div>
+        </div>
+      }
     </div>
   )
 }
