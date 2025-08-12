269af92f5d9edfa127aa1c564568f605a5904e2b
Tuan Le <leanhtuan030523@gmail.com>
2025-08-12 18:57:03 +0700

    ui(profile): restructure profile page with avatar, conditional FollowButton, and placeholder for follow dialog

--- frontend/src/routes/profile.$username.tsx
+++ frontend/src/routes/profile.$username.tsx
@@ -1,10 +1,13 @@
 import { getProfile } from '@/api/user';
-import FollowButton from '@/components/FollowButton';
+import FollowButton from '@/components/buttons/FollowButton';
 import FollowList from '@/components/FollowList';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { useAuth } from '@/contexts/auth';
+import type { UserProfile } from '@/types/user';
 import { createFileRoute } from '@tanstack/react-router'
+import { UserRound } from 'lucide-react';
 import { useState } from 'react';
 
 export const Route = createFileRoute('/profile/$username')({
@@ -16,7 +19,7 @@ export const Route = createFileRoute('/profile/$username')({
         }
         try {
             const res = await getProfile(username);
-            return { user: res.data, error: null };
+            return { user: res.data as UserProfile, error: null };
         } catch (error: any) {
             const errorMessage = error.response?.data?.detail || 'An error occurred';
             if (error.response?.status === 404) {
@@ -28,14 +31,14 @@ export const Route = createFileRoute('/profile/$username')({
 })
 
 function RouteComponent() {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, currentUser } = useAuth();
     const { user, error } = Route.useLoaderData();
 
-    const [activeTab, setActiveTab] = useState<string>('followers');
+    // const [activeTab, setActiveTab] = useState<string>('followers');
 
-    const handleTabChange = (tab: string) => {
-        setActiveTab(tab);
-    }
+    // const handleTabChange = (tab: string) => {
+    //     setActiveTab(tab);
+    // }
 
     if (error) {
         return (
@@ -46,21 +49,35 @@ function RouteComponent() {
     }
 
     return (
-        <div>
-            <h1>{user.name || user.username}'s Profile</h1>
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button type='button' onClick={() => handleTabChange('followers')}>128 followers</Button>
-                </DialogTrigger>
+        <div className='p-5 bg-red-300 min-h-svh w-svh'>
+            <div className='flex gap-5'>
+                <Avatar className='w-20 h-20'>
+                    <AvatarImage src={user?.avatar_url} />
+                    <AvatarFallback>
+                        <UserRound className='w-full h-full' />
+                    </AvatarFallback>
+                </Avatar>
+                <div>
+                    <p className='text-xl mb-4'>{user?.username}</p>
+                    {/* <Dialog>
+                        <DialogTrigger asChild>
+                            <Button type='button' onClick={() => handleTabChange('followers')}>128 followers</Button>
+                        </DialogTrigger>
 
-                <DialogTrigger asChild>
-                    <Button type='button' onClick={() => handleTabChange('followings')}>125 followings</Button>
-                </DialogTrigger>
+                        <DialogTrigger asChild>
+                            <Button type='button' onClick={() => handleTabChange('followings')}>125 followings</Button>
+                        </DialogTrigger>
 
-                <FollowList selectedTab={activeTab} onTabChange={handleTabChange} user_id={user.id} />
-            </Dialog>
-            {isAuthenticated && <FollowButton user={user}/>}
-            
+                        <FollowList selectedTab={activeTab} onTabChange={handleTabChange} user_id={user?.id} />
+                    </Dialog> */}
+                    {isAuthenticated &&
+                        <div className='flex gap-2'>
+                            <Button>Message</Button>
+                            <FollowButton user={user} />
+                        </div>
+                    }
+                </div>
+            </div>            
         </div>
     )
 }
