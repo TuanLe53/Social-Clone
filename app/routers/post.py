493bd1765c2c837632b6c82ad84e85f05767bc78
Tuan Le <leanhtuan030523@gmail.com>
2025-09-02 13:05:04 +0700

    Implement like/unlike functionality for posts: - POST /like/{post_id} to like a post - DELETE /like/{post_id} to unlike a post - 'GET /is_liked/{post_id}' to check if current user liked a post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,15 @@
 from typing import Annotated
-from fastapi import APIRouter, UploadFile, Form, Depends
+from fastapi import APIRouter, UploadFile, Form, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app.services.post import create_post as create_post_service, get_posts_by_user_id
+from app.services.post import (
+    create_post as create_post_service,
+    get_posts_by_user_id,
+    get_post_by_id,
+    like_post as like_post_service,
+    unlike_post as unlike_post_service,
+    has_liked_post
+)
 from app.db.models import User
 from app.dependencies import get_current_user
 from app.db.database import get_db
@@ -44,3 +51,30 @@ async def create_post(user: Annotated[User, Depends(get_current_user)], content:
 async def get_posts_by_user(user_id: str, limit: int = 10, offset: int = 0, db: Session = Depends(get_db)):
     posts = get_posts_by_user_id(db_session=db, user_id=user_id, limit=limit, offset=offset)
     return posts
+
+@router.get("/is_liked/{post_id}")
+async def is_liked(post_id: str, user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+    post = get_post_by_id(db, post_id)
+    is_liked = has_liked_post(post, user)
+    
+    return {"is_liked": is_liked}
+
+@router.post("/like/{post_id}", response_model=PostSchema)
+async def like_post(post_id: str, user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+    post = get_post_by_id(db, post_id)
+    if not post:
+        raise HTTPException(status_code=400, detail="Post not found")
+    
+    updated_post = like_post_service(db, post, user)
+    
+    return updated_post
+
+@router.delete("/like/{post_id}", response_model=PostSchema)
+async def unlike_post(post_id: str, user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+    post = get_post_by_id(db, post_id)
+    if not post:
+        raise HTTPException(status_code=400, detail="Post not found")
+    
+    updated_post = unlike_post_service(db, post, user)
+    
+    return updated_post
